e1ada54b2f3ba701bd416e7bb1d6e5c94bf60cb7
PleasantGoat84 <PleasantGoat84@gmail.com>
2020-08-25 11:49:46 +0800

    adjust style and make the list selectable

--- src/components/home/TopSmeList.vue
+++ src/components/home/TopSmeList.vue
@@ -5,19 +5,21 @@
     <v-card-title class="justify-center">熱門中小企</v-card-title>
 
     <v-card-text>
-      <v-list outlined rounded>
-        <template v-for="(sme, i) in topSmes">
-          <v-list-item :key="i">
-            <v-list-item-icon class="ml-n2 mr-4 justify-center">
-              <v-icon color="#ffb100" v-if="i === 0">mdi-crown</v-icon>
-              <span v-else class="subtitle-2">{{ i + 1 }}. </span>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ sme.name }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider :key="`divider-${i}`" v-if="i < topSmes.length - 1" />
-        </template>
+      <v-list outlined class="py-0">
+        <v-list-item-group>
+          <template v-for="(sme, i) in top3Smes">
+            <v-list-item :key="i">
+              <v-list-item-icon class="ml-n2 mr-4 justify-center">
+                <v-icon color="#ffb100" v-if="i === 0">mdi-crown</v-icon>
+                <span v-else class="subtitle-2">{{ i + 1 }}. </span>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ sme.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider :key="`divider-${i}`" v-if="i < top3Smes.length - 1" />
+          </template>
+        </v-list-item-group>
       </v-list>
     </v-card-text>
 
@@ -42,7 +44,12 @@ export default {
         name: "大順行凍肉食品有限公司"
       }
     ]
-  })
+  }),
+  computed: {
+    top3Smes: function() {
+      return this.topSmes.slice(0, 3);
+    }
+  }
 };
 </script>
 
